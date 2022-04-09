8eed2002217a8a8e33b75edbb4e2ed5109bdb169
Jude Cooray <ranhiru@gmail.com>
2022-04-10 09:39:58 +1000

    Added API endpoint to submit user submissions

--- spec/requests/api/slamdunk/submissions_spec.rb
+++ spec/requests/api/slamdunk/submissions_spec.rb
@@ -1,7 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Slamdunk::Submissions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  path '/api/slamdunk/submit' do
+    post('increment upvotes') do
+      consumes 'application/json'
+      produces 'application/json'
+
+      parameter name: :submission, in: :body, schema: {
+        type: :object,
+        properties: {
+          station_id: {
+            type: :integer,
+            required: true
+          },
+          gas_type: {
+            type: :string,
+            enum: ["petrol_93", "diesel", "petrol_95", "gas_cylinder", "super_diesel"],
+            required: true
+          },
+          availability: {
+            type: :string,
+            enum: ["available", "unavailable", "unsure"],
+            required: true
+          },
+          wait_time: { type: :integer },
+          submitted_at: { type: "string", required: true }
+        }
+      }
+
+      response(200, 'successful') do
+        let(:submission) {
+          {
+            "submission": {
+              "station_id": 2,
+              "gas_type": "petrol_93",
+              "availability": "unavailable",
+              "wait_time": 10,
+              "submitted_at": "2022-04-09T21:47:36.923Z"
+            }
+          }
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
