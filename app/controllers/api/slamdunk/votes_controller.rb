b2e06e7f3cca7cd2ed9a31f0d900aeb660f9bfff
Arjun Balasingam <arjunvb@mit.edu>
2022-04-09 15:03:50 -0700

    Remove extra puts params

--- app/controllers/api/slamdunk/votes_controller.rb
+++ app/controllers/api/slamdunk/votes_controller.rb
@@ -1,13 +1,11 @@
 class Api::Slamdunk::VotesController < ApplicationController
     def upvote
-        puts params
         entry = UserSubmission.find(station_id: params["id"])
         entry.increment!(:upvotes)
         render json: {}
     end
 
     def downvote
-        puts params
         entry = UserSubmission.find(station_id: params["id"])
         entry.increment!(:downvotes)
         render json: {}
